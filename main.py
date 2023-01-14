bfd4b3a29d9e5f2e6ea3f0b365568cf2b0d573d4
Gal Zafar <theycallmefachfach@gmail.com>
2023-01-15 01:00:34 +0200

    add text processing on post insert;
    posts are only inserted if not already exist;
    
    add preprocessed text database column for a post
    
    some other not yet needed junk to the analysis file

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 import praw
+
+from analysis import preprocess
+
 load_dotenv()
 
 url: str = os.getenv("SUPABASE_URL")
@@ -23,20 +26,53 @@ print(redditClient.user.me())
 subreddit = redditClient.subreddit("ProgrammingBuddies")
 
 def is_looking_for_buddies_flare(submission) -> bool: # TODO: add type hinting
-    return submission.link_flair_text.lower() == "LOOKING FOR BUDDIES".lower()
-
-number_of_posts: int = 0
-for submission in subreddit.stream.submissions():
     if submission.link_flair_text is None:
-        continue
-    elif is_looking_for_buddies_flare(submission):
-        number_of_posts += 1
-        print(f'number of posts: {number_of_posts}')
-        print(f'New post: {submission} \n')
-        print(f"submission: {redditClient.submission(id=submission.id)}")
+        return False
     else:
-        continue
-    # show post properties
+        return submission.link_flair_text.lower() == "LOOKING FOR BUDDIES".lower()
 
 
+def run_posts_stream_processing():
+    print('running stream processing')
+    for submission in subreddit.stream.submissions():
+        if is_looking_for_buddies_flare(submission):
+            print(f'post: {submission} \n')
+
+            body: str = submission.selftext
+            text: str = submission.title + " " + body
+
+            post_info = {
+                "title": submission.title,
+                "url": submission.url,
+                "body": body,
+                "processed_text": preprocess(text),
+                "created": submission.created_utc,
+                "post_id": submission.id,
+                "flair": submission.link_flair_text,
+                "author": submission.author.name,
+            }
+            
+
+            data = supabase.table("Posts").select("post_id").eq("post_id", submission.id).execute()
+
+            if len(data.data) == 0:        
+                print('inserting post')
+                print(f"post info: {post_info}")
+                supabase.table("Posts").insert(
+                    post_info
+                ).execute()
+
+            else:
+                print('post already exists')
+                print(f"data: {data}")
+
+
+
+
+def main():
+    run_posts_stream_processing()
+
+
+if __name__ == "__main__":
+    main()
 
