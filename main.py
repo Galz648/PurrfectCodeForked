2b8097c21d062426e6f546777ab6bf82993ed25b
Gal Zafar <theycallmefachfach@gmail.com>
2023-01-15 01:16:28 +0200

    refactor indented code into functions

--- main.py
+++ main.py
@@ -32,39 +32,44 @@ def is_looking_for_buddies_flare(submission) -> bool: # TODO: add type hinting
         return submission.link_flair_text.lower() == "LOOKING FOR BUDDIES".lower()
 
 
+def post_exists(post_id: str) -> bool:
+    data = supabase.table("Posts").select("post_id").eq("post_id", post_id).execute()
+    return len(data.data) > 0
+
+
+def insert_post(post_info: dict) -> bool:
+    data = supabase.table("Posts").insert(post_info).execute()
+    return data.status_code == 201
+
+def create_post_info(submission) -> dict: # TODO: add type hinting
+    body: str = submission.selftext
+    text: str = submission.title + " " + body
+
+    post_info = { # TODO: extract this into a function
+        "title": submission.title,
+        "url": submission.url,
+        "body": body,
+        "processed_text": preprocess(text),
+        "created_time_utc": submission.created_utc,
+        "post_id": submission.id,
+        "flair": submission.link_flair_text,
+        "author": submission.author.name,
+    }
+    return post_info
+    
 def run_posts_stream_processing():
     print('running stream processing')
     for submission in subreddit.stream.submissions():
         if is_looking_for_buddies_flare(submission):
             print(f'post: {submission} \n')
 
-            body: str = submission.selftext
-            text: str = submission.title + " " + body
-
-            post_info = {
-                "title": submission.title,
-                "url": submission.url,
-                "body": body,
-                "processed_text": preprocess(text),
-                "created": submission.created_utc,
-                "post_id": submission.id,
-                "flair": submission.link_flair_text,
-                "author": submission.author.name,
-            }
-            
-
-            data = supabase.table("Posts").select("post_id").eq("post_id", submission.id).execute()
-
-            if len(data.data) == 0:        
-                print('inserting post')
-                print(f"post info: {post_info}")
-                supabase.table("Posts").insert(
-                    post_info
-                ).execute()
+            if not post_exists(submission.id):
+                post_info = create_post_info(submission)
+                is_created  = insert_post(post_info)
+                print('post created: ', is_created)
 
             else:
                 print('post already exists')
-                print(f"data: {data}")
 
 
 
